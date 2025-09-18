b18b1d12405e63b84dcfce458110017511eb9115
vasc-language <vasc-language@users.noreply.github.com>
2025-09-18 13:53:46 +0800

    fix: 安装 tslib 以解决 openapi 生成失败

--- picture-frontend/src/layouts/BasicLayout.vue
+++ picture-frontend/src/layouts/BasicLayout.vue
@@ -1,19 +1,46 @@
 <template>
   <div id="basicLayout">
     <a-layout style="min-height: 100vh">
-      <a-layout-header>Header</a-layout-header>
-      <a-layout-content>Content</a-layout-content>
-      <a-layout-footer>Footer</a-layout-footer>
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/vasc-language/picture" target="_blank">
+          项目地址 By vasc-language
+        </a>
+      </a-layout-footer>
     </a-layout>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import GlobalHeader from '@/components/GlobalHeader.vue'
 </script>
 
 <style scoped>
-#basicLayout {
+#basicLayout .header {
+  padding-inline: 20px;
+  background: white;
+  color: unset;
+  margin-bottom: 16px;
+}
 
+#basicLayout .content {
+  padding: 20px;
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
 }
 </style>
